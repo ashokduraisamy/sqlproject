64d93a0d1260c3f3823e1300cf0a67f4e89953bd
ashokduraisamy <120883903+ashokduraisamy@users.noreply.github.com>
2023-02-03 16:15:50 +0530

    Update company details.sql

--- company details.sql
+++ company details.sql
@@ -53,3 +53,27 @@ into amazing_tech VALUES (2428,'ram','ram@gmail.com',9600498889,'28-07-75',21000
 into amazing_tech VALUES (2429,'karthi','karthi@gmail.com',9566027982,'20-11-89',17000,  985476365214,70)
 into amazing_tech VALUES (2430,'buvanan','buvanan@gmail.com',9585899116,'25-08-02',15000,951236874125,80)
 select * from dual ;
+
+create table department_id 
+ (
+ departmentid number not null,
+ jobdesc VARCHAR2 (150),
+ d_salary NUMBER,
+ CONSTRAINT d_1 primary key (departmentid)
+ );
+ 
+ drop table department_id;
+ 
+ select * from department_id ;
+ 
+ insert all
+  into department_id VALUES (10,'manager',30000)
+  into department_id VALUES (20,'hr',27000)
+  into department_id VALUES (30,'Administration',25000) 
+  into department_id VALUES (40,'Purchasing',23000)
+  into department_id VALUES (50,'Marketing',21000)
+  into department_id VALUES (60,'Shipping',19000)
+  into department_id VALUES (70,'Human Resources',17000) 
+  into department_id VALUES (80,'Sales',15000)
+  into department_id VALUES (90,'IT',10000)
+ select * from dual ;  
